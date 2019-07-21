1f75af635d82ffe1bbabfa8613a117fb826437bc
Anna Basova <annabassova152@gmail.com>
2019-07-21 22:32:30 +0300

    Create endpoint for film details

--- src/views/movies.py
+++ src/views/movies.py
@@ -1,11 +1,11 @@
+import bson
 from flask_restplus import Resource, fields, marshal
 
 from ..server import MovieNamespace, api
 from ..services import movies as MovieService
 
-
 # Request
-MovieCreateModel = api.model('MovieCreate', {
+MovieRequestModel = api.model('MovieRequest', {
     'title': fields.String(required=True),
     'genres': fields.List(fields.String(), required=True)
 })
@@ -25,10 +25,39 @@ class MoviesList(Resource):
     def get(self):
         return MovieService.find()
 
-    @MovieNamespace.expect(MovieCreateModel, validate=True)
+    @MovieNamespace.expect(MovieRequestModel, validate=True)
     @MovieNamespace.marshal_with(MovieModel)
     def post(self):
-        data = marshal(api.payload, MovieCreateModel)
+        data = marshal(api.payload, MovieRequestModel)
 
         _id = MovieService.create(data)
-        return MovieService.find_by_id(str(_id))
+        return MovieService.find_by_id(_id)
+
+
+@MovieNamespace.route('/<string:pk>/')
+class MovieDetails(Resource):
+    @MovieNamespace.marshal_with(MovieModel)
+    def get(self, pk):
+        try:
+            return MovieService.find_by_id(pk)
+        except (ValueError, bson.errors.InvalidId):
+            MovieNamespace.abort(
+                404, 'Movie with _id \'{}\' not found '.format(pk))
+
+    @MovieNamespace.expect(MovieRequestModel)
+    @MovieNamespace.marshal_with(MovieModel)
+    def patch(self, pk):
+        try:
+            MovieService.update(pk, api.payload)
+            return MovieService.find_by_id(pk)
+        except (ValueError, bson.errors.InvalidId):
+            MovieNamespace.abort(
+                404, 'Movie with _id \'{}\' not found '.format(pk))
+
+    def delete(self, pk):
+        try:
+            MovieService.delete(pk)
+            return None, 204
+        except (ValueError, bson.errors.InvalidId):
+            MovieNamespace.abort(
+                404, 'Movie with _id \'{}\' not found '.format(pk))
