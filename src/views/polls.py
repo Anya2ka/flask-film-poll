910162acaa7a2c6643dc7e63b9ddd102c62e25b6
Anna Basova <annabassova152@gmail.com>
2019-07-26 19:03:17 +0300

    Create voting endpoint

--- src/views/polls.py
+++ src/views/polls.py
@@ -21,6 +21,11 @@ PollRequestModel = api.model('PollRequest', {
     'movies': fields.Raw(example={'<id>': []})
 })
 
+# Request
+ValueRequestModel = api.model('ValueRequest', {
+    'value': fields.Integer(required=True)
+})
+
 # Response
 PollModel = api.model('Poll', {
     '_id': fields.String(required=False, readonly=True),
@@ -74,3 +79,21 @@ class PollDetails(Resource):
         except (ValueError, bson.errors.InvalidId):
             PollNamespace.abort(
                 404, 'Poll with _id \'{}\' not found '.format(pk))
+
+
+@PollNamespace.route('/<string:pk>/<string:movie_pk>/')
+class MovieValue(Resource):
+    @PollNamespace.expect(ValueRequestModel)
+    @PollNamespace.marshal_with(PollModel)
+    def post(self, pk, movie_pk):
+        data = api.payload["value"]
+        if data < 1 or data > 10:
+            PollNamespace.abort(
+                400, "Unsupported value")
+        try:
+            PollService.update_by_details(pk, movie_pk, data)
+            return PollService.find_by_id(pk)
+        except ValueError as error:
+            PollNamespace.abort(404, error.args[0])
+        except bson.errors.InvalidId as error:
+            PollNamespace.abort(400, error.args[0])
